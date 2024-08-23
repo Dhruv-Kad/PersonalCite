96ebffb49f5709e69a6afd5844c349b670bba18e
Dhruv Kadam(XBOOK) <dhruv@minitri>
2024-08-23 18:19:30 -0500

    First commit in a while. Worked on webget function and after many errors it almost finally works. Nearly deleted the whole file, so now I'm uploading to git.

---
+++ webget.py
@@ -0,0 +1,64 @@
+# This is the part that should get information from the web so that the user has a more streamlined experince
+from bs4 import  BeautifulSoup
+import requests
+infolist = ["SiteName", "ArticleTitle", "ArticlePublishedTime", "ArticlePublisher", "ArticleAuthor"]
+infodict = {}
+#probably going to need to come up with some redanduancy for all the different ways that metadata can be associated
+#never mind
+def getinfo(url):
+    matchtable = ["og:site_name", "og:title", "article:published_time", "article:publisher", "article:author"]
+    source = requests.get(url).text
+    soup = BeautifulSoup(source, 'lxml')
+    inflist_counter = 0
+    for i in matchtable:
+        match1 = soup.find('meta', property= (i))
+        infodict[infolist[inflist_counter]] = match1
+        inflist_counter += 1
+    print(infodict)
+    return(prettify(infodict))
+
+def prettify(uglydict):
+    print("PRETTYSTARTED")
+    pretdict ={}
+    for i in uglydict:
+        uglyval = ""
+        uglyval = str(uglydict[i])
+        binaryno = 0
+        prettyval = ""
+        print(pretdict)
+        for i in uglyval:
+            if(i == '"'):
+                binaryno+=1
+                if(binaryno == 2):
+                    pretdict[i] = prettyval
+                    break
+            if(binaryno%2  == 1 ):
+                prettyval += i 
+                
+    return pretdict 
+    
+
+def parseinfo(url): 
+        #Make a list with the key parts of a citation formatted pretty
+    allreqs = [
+            "SiteName",
+            "ArticleTitle",
+            "ArticlePublishedTime",
+            "ArticlePublisher",
+            "ArticleAuthor",
+            "SiteUrl" ,
+            "AccessDate",
+            "Other contributers"
+    ]
+
+    foundinfo = getinfo(url)
+    y=0
+    allreqdict = {}
+    for x in foundinfo:
+        val = foundinfo[x]
+        allreqdict[allreqs[y]] = val
+        y += 1 
+    print(allreqdict)
+
+if __name__ == '__main__':
+    print(parseinfo('https://www.thestack.technology/microsofts-new-ai-pcs-will-screenshot-everything-every-2-seconds/'))
